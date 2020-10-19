efdbbbaaa1388f32ed4f698a66496dd4df6fdeb2
Plimon Howard <plimon.howard@gmail.com>
2020-10-19 22:33:23 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,6 +13,6 @@ def create_project_hash
       :location => project.css("ul.project-meta span.location-name").text,
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
-
+  end
   projects
 end
